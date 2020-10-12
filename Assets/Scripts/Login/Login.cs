3468c1b9fe381e0169c5d7d2300ccbbb9d81ea2f
Duchuy <huy.hd@rubycell.com>
2020-10-12 19:59:42 +0700

    init

---
+++ Assets/Scripts/Login/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    public TMP_InputField txtName;
+    public void OnLogin()
+    {
+        GameSystem.clientInfo.username = txtName.text;
+    }
+}
