4f83e9f4b833f1efd9dfb47fa311fbb670c10e9e
Huỳnh Đức Huy <duchuy.huynh.413@gmail.com>
2020-10-27 09:08:02 +0700

    dragtouse

---
+++ Assets/_Game/Scripts/UI/ItemDragToUse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragToUse : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public ItemDetailDisplay itemDetail;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
